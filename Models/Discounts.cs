58520020ea388c106a6ed8205e05399099bd0ba3
育瑄 張 <kellyjhang8329@gmail.com>
2018-05-09 16:29:53 +0800

    更新

---
+++ Models/Discounts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MVCSolution.OnlineStore.Models
+{
+    public class Discounts
+    {
+        public int DiscountID { get; set; }
+        public int ProductID { get; set; }
+        public int Discount { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+    }
+}
---
+++ Models/PS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MVCSolution.OnlineStore.Models
+{
+    public class PS
+    {
+        public int SizeID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
