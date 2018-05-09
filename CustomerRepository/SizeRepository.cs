349e8332cd80b452519e87588ffa8aff88bf08de
耀文 郭 <karta1407617@gmail.com>
2018-05-09 18:18:24 +0800

    新增連接字串 新增單元測試

---
+++ CustomerRepository/SizeRepository.cs
@@ -0,0 +1,93 @@
+﻿using BuildSchool.MVCSolution.OnlineStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MVCSolution.OnlineStore.CustomerRepository
+{
+    public class SizeRepository
+    {
+        public void Create(Sizes model)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=myDataBase;Trusted_Connection=True;");
+
+            var sql = "INSERT INTO Orders(@SizeID , Size) Values(@sizeid , @size)";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@sizeid", model.SizeID);
+            command.Parameters.AddWithValue("@size", model.Size);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+
+
+
+        public void Update(Sizes model)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=myDataBase;Trusted_Connection=True;");
+
+            var sql = "UPDATE Orders(@SizeID , Size) SET(SizeID = @sizeid , Size = @size)";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@sizeid", model.SizeID);
+            command.Parameters.AddWithValue("@size", model.Size);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+        public void Delete(Sizes model)
+        {
+
+            SqlConnection connection = new SqlConnection("Server=.;Database=myDataBase;Trusted_Connection=True;");
+
+            var sql = "UPDATE Orders(@SizeID , Size) SET(SizeID = @sizeid , Size = @size)";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@sizeid", model.SizeID);
+            command.Parameters.AddWithValue("@size", model.Size);
+
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public IEnumerable<Sizes> GetAll() //()內不用給直 因為傳整個表格
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=mytest;Trusted_Connection=True;");
+
+            var sql = "SELECT * FROM  Sizes";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+           // command.Parameters.AddWithValue("@sizeID", model.SizeID);
+
+            connection.Open();
+
+            var reader =  command.ExecuteReader();
+            var list = new List<Sizes>();
+            var sizes = new Sizes();
+            while (reader.Read())
+            {
+                sizes.SizeID = int.Parse(reader.GetValue(reader.GetOrdinal("sizeID")).ToString());
+                sizes.Size = reader.GetValue(reader.GetOrdinal("size")).ToString();
+                list.Add(sizes);
+            }
+            reader.Close();
+            connection.Close();
+
+            return list;
+        }
+
+
+
+    }
+}
---
+++ CustomerRepositoryTests/SizeRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuildSchool.MVCSolution.OnlineStore.CustomerRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MVCSolution.OnlineStore.CustomerRepository.Tests
+{
+    [TestClass()]
+    public class SizeRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            var repository = new BuildSchool.MVCSolution.OnlineStore.CustomerRepository.SizeRepository();
+            var list = repository.GetAll();
+            Assert.IsTrue(list.Count() > 0);
+        }
+    }
+}
