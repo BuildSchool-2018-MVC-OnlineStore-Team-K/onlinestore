31e80c80630c195f8da170473f35d7cf9d91deec
Jay <shadle0307@gmail.com>
2018-05-09 16:32:53 +0800

    更新

---
+++ Models/Sizes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MVCSolution.OnlineStore.Models
+{
+    public class Sizes
+    {
+        public int SizeID { get; set; }
+        public string Size { get; set; }
+    }
+}
