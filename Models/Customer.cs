911dfc0edc233ee68a9e47b87562a40126e60351
耀文 郭 <karta1407617@gmail.com>
2018-05-09 16:05:31 +0800

    新增Orders

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace BuildSchool.MVCSolution.OnlineStore.Models
 {
-    public class Customer
+    public class Orders
     {
+        public int OrderID { get; set; }
+        public int MemberID { get; set; }
+        public int OrderDetailID { get; set; }
+        public int Cart { get; set; }
+        public int Pay { get; set; }
+        public DateTime Time { get; set; }
+        public string ShipPlace { get; set; }
+        public string Payway { get; set; }
+
     }
 }
