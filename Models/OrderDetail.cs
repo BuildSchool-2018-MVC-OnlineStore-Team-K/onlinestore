b2f2f6317ae32f97915acdc409c2befe9e6e7fcc
陳書慕 <d757821137@gmail.com>
2018-05-10 14:15:55 +0800

    更新OrderDetail Discount的欄位型別

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -12,6 +12,6 @@ namespace BuildSchool.MVCSolution.OnlineStore.Models
         public int ProductID { get; set; }
         public int UnitPrice { get; set; }
         public int Quantity { get; set; }
-        public int Discount { get; set; }
+        public float Discount { get; set; }
     }
 }
