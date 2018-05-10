81063491cc841b3956f6ed6e4eac26a5d2a39b63
耀文 郭 <karta1407617@gmail.com>
2018-05-10 11:59:59 +0800

    更新

--- RepositoryTests/SizeRepositoryTests.cs
+++ RepositoryTests/SizeRepositoryTests.cs
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BuildSchool.MVCSolution.OnlineStore.Repository;
 using System;
-=======
-﻿using System;
->>>>>>> e8a299becf2cdce0fe6a46f19e880e9e58d677fd
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BuildSchool.MVCSolution.OnlineStore.Models;
 using System.Linq;
 
-<<<<<<< HEAD
 namespace BuildSchool.MVCSolution.OnlineStore.SizeRepositoryTests
-=======
-namespace BuildSchool.MVCSolution.OnlineStore.SizeRepositoryTest
->>>>>>> e8a299becf2cdce0fe6a46f19e880e9e58d677fd
-{
+{ 
     [TestClass]
     public class SizeRepositoryTests
     {
         [TestMethod]
         public void GetAll()
         {
-<<<<<<< HEAD
             var repository = new BuildSchool.MVCSolution.OnlineStore.Repository.SizeRepository();
-=======
-            var repository = new BuildSchool.MVCSolution.OnlineStore.SizeRepository.SizeRepository();
->>>>>>> e8a299becf2cdce0fe6a46f19e880e9e58d677fd
             var list = repository.GetAll();
             Assert.IsTrue(list.Count() > 0);
         }
