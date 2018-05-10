caba92e2efc5ad9fc38fcd786f63d4318d2a169d
Jay <shadle0307@gmail.com>
2018-05-10 15:31:36 +0800

    Discount:float -> double Test: AddNew

---
+++ RepositoryTests/DiscountsRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuildSchool.MVCSolution.OnlineStore.Repository;
+using System;
+using System.Collections.Generic;
+using BuildSchool.MVCSolution.OnlineStore.Models;
+using System.Linq;
+
+namespace BuildSchool.MVCSolution.OnlineStore.DicountsRepositoryTests
+{
+    [TestClass]
+    public class DiscountsRepositoryTests
+    {
+        [TestMethod]
+        public void DiscountsGetAll()
+        {
+            var repository = new BuildSchool.MVCSolution.OnlineStore.Repository.DiscountRepository();
+            var list = repository.GetAll();
+            Assert.IsTrue(list.Count() > 0);
+        }
+    }
+}
