b1d116aa7a156d3fce92eb61b1c38e10864a8da2
柏呈 崔 <EREFDDD@GMAIL.COM>
2018-05-10 14:39:47 +0800

    Test

---
+++ RepositoryTests/MembersRepositoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MVCSolution.OnlineStore.MembersRepositoryTests
+{
+    [TestClass()]
+    public class MembersRepositoryTest
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            var repository = new BuildSchool.MVCSolution.OnlineStore.Repository.MembersRepository();
+            var result = repository.GetAll();
+            Assert.IsTrue(result.Count() > 0);
+        }
+    }
+}
