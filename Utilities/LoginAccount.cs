d1cebbd1179730a2fb5a058ad7f1c4778646c704
柏呈 崔 <EREFDDD@GMAIL.COM>
2018-05-12 15:20:29 +0800

    未完成

---
+++ Utilities/LoginAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MVCSolution.OnlineStore.Utilities
+{
+  public class LoginAccount<Members> where Members : class, new()
+    {
+        public static Members Bind
+    }
+}
