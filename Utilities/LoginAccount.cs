d0c2457cf1594507b5f202d94d3c7f278bc03512
柏呈 崔 <EREFDDD@GMAIL.COM>
2018-05-12 16:12:04 +0800

    未完成

--- Utilities/LoginAccount.cs
+++ Utilities/LoginAccount.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace BuildSchool.MVCSolution.OnlineStore.Utilities
 {
-  public class LoginAccount<Members> where Members : class, new()
+    public class LoginAccount<Members> where Members : class, new()
     {
-        public static Members Bind
+
+        public int Acc, Pass;
+        public LoginAccount(int Acc, int Pass)
+        {
+            var repository = new BuildSchool.MVCSolution.OnlineStore.Repository.MembersRepository();
+            this.Acc = repository.Account();
+            this.Pass = repository.Password();
+
+        }
     }
 }
