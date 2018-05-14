41bd6944ef2d132a13aab6f003475f102eee34df
耀文 郭 <karta1407617@gmail.com>
2018-05-14 17:44:41 +0800

    Abstract

---
+++ Abstracts/ISaltStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.PasswordValidationTool.Abstracts
+{
+    public interface ISaltStrategy
+    {
+        string Format(string passwordBody);
+        byte[] Format(byte[] passwordData);
+    }
+}
---
+++ Implementation/SaltStrategy/DefaultSaltStrategy.cs
@@ -0,0 +1,22 @@
+﻿using BuildSchool.PasswordValidationTool.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.PasswordValidationTool.Implementation.SaltStrategy
+{
+    public class DefaultSaltStrategy : ISaltStrategy
+    {
+        public string Format(string passwordBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Format(byte[] passwordData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
