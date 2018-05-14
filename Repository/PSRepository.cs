35c658580bec45864cf2326033e426f69fb22ac5
育瑄 張 <kellyjhang8329@gmail.com>
2018-05-15 00:57:33 +0800

    新增Size_GetAllDapper&PS_GetAllDapper

--- Repository/PSRepository.cs
+++ Repository/PSRepository.cs
@@ -120,5 +120,14 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
 
             return list;
         }
+
+        //擴充套件Dapper
+        public IEnumerable<PS> PS_GetAllDapper()
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source = 192.168.40.36,1433 ; database = E-Commerce ; user id = smallhandsomehandsome; password = 123");
+            var result = connection.Query<PS>("SELECT * FROM PS");
+            return result;
+        }
     }
 }
--- Repository/SizeRepository.cs
+++ Repository/SizeRepository.cs
@@ -116,5 +116,14 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
 
             return list;
         }
+
+        //擴充套件Dapper
+        public IEnumerable<Size> Size_GetAllDapper()
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source = 192.168.40.36,1433 ; database = E-Commerce ; user id = smallhandsomehandsome; password = 123");
+            var result = connection.Query<Size>("SELECT * FROM Size");
+            return result;
+        }
     }
 }
