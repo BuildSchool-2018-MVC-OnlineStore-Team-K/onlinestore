140880f6cf2689ef2d4443328b576f9b41749775
丰羿 王 <tra202temu2000@gmail.com>
2018-05-15 12:11:55 +0800

    變更

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BuildSchool.PasswordValidationTool.Abstracts;
 using BuildSchool.PasswordValidationTool.Implementation.HashingProvider;
 using BuildSchool.PasswordValidationTool.Implementation.PasswordRules;
 using BuildSchool.PasswordValidationTool.Implementation.SaltStrategy;
 using Client;
+using SimpleInjector;
 
 namespace BuildSchoolPassword.ValidationToolClient.Client
 {
@@ -14,10 +17,19 @@ namespace BuildSchoolPassword.ValidationToolClient.Client
     {
         static void Main(string[] args)
         {
-            var service = new PasswordValidationService(
-                new SHA512HashingProvider(), 
-                new DefaultSaltStrategy(), 
-                new HighComplexityPasswordRule());
+            //redister dependency
+            var container = new SimpleInjector.Container();
+            container.Register<IHashingProvider, SHA512HashingProvider>();
+            container.Register<IPasswordRule, MediumComplexityPasswordRule>();
+            container.Register<ISaltStrategy, DefaultSaltStrategy>();
+            container.Register<IPasswordValidationService, PasswordValidationService>();
+
+            //resolve object and get instance
+            //var service = new PasswordValidationService(
+            //    new SHA512HashingProvider(), 
+            //    new DefaultSaltStrategy(), 
+            //    new MediumComplexityPasswordRule());
+            var service = container.GetInstance<IPasswordValidationService>();
             //var pwd = service.GeneratePassword();
             var userInputPwd = "x3n84tNe";
             var storedPwd = "x3n84tNe";
