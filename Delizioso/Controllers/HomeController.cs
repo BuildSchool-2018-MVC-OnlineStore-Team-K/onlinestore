1003b2e03c4d0cd3413eda5ae1fd8d4bf21b36d9
育瑄 張 <kellyjhang8329@gmail.com>
2018-05-23 14:15:31 +0800

    新增Controllers

---
+++ Delizioso/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using BuildSchool.MVCSolution.OnlineStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    
+    public class HomeController
+    {
+       
+    }
+}
---
+++ Delizioso/Controllers/OrderDetailsController.cs
@@ -0,0 +1,32 @@
+﻿//using Deliozo.NorthWindModels;
+using BuildSchool.MVCSolution.OnlineStore.Repository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("orders")]
+    public class OrderDetailsController
+    {
+        [Route("OrderDetails")]
+        public ActionResult OrderDetailsViews()
+        {
+
+            var db = new OrdersRepository();
+            var query = db.GetAll();
+
+            return View(query);
+        }
+
+        private ActionResult View(IEnumerable<Orders> query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
