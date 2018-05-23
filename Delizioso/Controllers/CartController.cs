1bf3e0d529cab2db105b850f4283e4a96eb9f854
耀文 郭 <karta1407617@gmail.com>
2018-05-23 15:55:42 +0800

    完成購物車Controller,Service

--- Delizioso/Controllers/CartController.cs
+++ Delizioso/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿//using Deliozo.NorthWindModels;
 using BuildSchool.MVCSolution.OnlineStore.Repository;
 using Newtonsoft.Json;
+using Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,9 @@ namespace WebApplication1.Controllers
         public ActionResult modeltest()
         {
 
-            var db = new OrdersRepository();
-            var query =  db.GetAll();
-            
+            var CartService1 = new CartService();
+            var query = CartService1.GetCartProducts(1 , 6);
+
             return View(query);
         }
 
---
+++ ViewModels/CartViewMOdel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    //  [Time] ,p.ProductName, Quantity , Total , p.ProductID , p.UnitPrice , sc.Stock , sc.Color , sz.SizeType , p.Category 
+    public class CartViewModel
+    {
+        public string ProductName  { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
