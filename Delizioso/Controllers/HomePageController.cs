c2e73dcb085dc5919cf7745275a47d171ef093dd
丰羿 王 <tra202temu2000@gmail.com>
2018-05-23 16:31:25 +0800

    新增產品照片欄位

--- Delizioso/Controllers/HomePageController.cs
+++ Delizioso/Controllers/HomePageController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BuildSchool.MVCSolution.OnlineStore.Models;
+using BuildSchool.MVCSolution.OnlineStore.Repository;
 
 namespace WebApplication1.Controllers
 {
     public class HomePageController : Controller
     {
         // GET: HomePage
-        public ActionResult Index()
+        //[Route("db.OrderByShelfTime")]
+        public ActionResult OrderByShelfTimeDESC()
         {
-            return View();
+            var db = new ProductsRepository();
+            var query = db.OrderByShelfTimeDESC();
+            return PartialView(query);
         }
     }
 }
