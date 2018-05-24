b7cb77885116f17f5d37dee45b9b1464a5c70e5a
耀文 郭 <karta1407617@gmail.com>
2018-05-24 18:14:02 +0800

    orderdetail

--- Repository/OrderDetailRepository.cs
+++ Repository/OrderDetailRepository.cs
@@ -137,7 +137,8 @@ namespace BuildSchool.MVCSolution.OnlineStore.OrderDetailRepository
         public IEnumerable<OrderDetailViewModel> GetMemberOrderDetail(int MemberID)
         {
             SqlConnection connection = new SqlConnection(connect);
-            return connection.Query<OrderDetailViewModel>("SELECT o.Time,o.OrderID,o.Payway,od.UnitPrice,od.Quantity,od.Discount FROM Members m INNER JOIN Orders o ON o.MemberID = m.MemberID INNER JOIN OrderDetail od ON od.OrderID = o.OrderID INNER JOIN Products p ON p.ProductID = od.ProductID WHERE m.MemberID = @MemberID AND o.Cart = 0", new
+            return connection.Query<OrderDetailViewModel>("SELECT o.Time,o.OrderID,o.Payway,od.UnitPrice,od.Quantity,od.Discount FROM Members m INNER JOIN Orders o ON o.MemberID = m.MemberID INNER JOIN OrderDetail od ON od.OrderID = o.OrderID INNER JOIN Products p ON p.ProductID = od.ProductID WHERE m.MemberID = @MemberID AND o.Cart = 0", 
+            new
             {
                 MemberID
             });
