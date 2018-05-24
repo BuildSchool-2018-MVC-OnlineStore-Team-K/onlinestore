4f22d598c0ae85d6340a6f48e0e69ec05f10d711
丰羿 王 <tra202temu2000@gmail.com>
2018-05-24 17:37:01 +0800

    HomeService新增

--- Delizioso/Controllers/HomeController.cs
+++ Delizioso/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BuildSchool.MVCSolution.OnlineStore.Repository;
 
 namespace WebApplication1.Controllers
 {
@@ -12,7 +13,7 @@ namespace WebApplication1.Controllers
         [Route("")]
         // GET: Home
         public ActionResult Home()
-        {
+        {          
             return View();
         }
     }
