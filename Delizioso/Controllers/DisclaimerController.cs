d2f87d91b1a611542664085f514a21674f16be41
育瑄 張 <kellyjhang8329@gmail.com>
2018-05-24 16:55:39 +0800

    新增會員中心還有免責聲明

---
+++ Delizioso/Controllers/DisclaimerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("Disclaimer")]
+    public class DisclaimerController : Controller
+    {
+        [Route("")]
+        // GET: Disclaimer
+        public ActionResult Disclaimer()
+        {
+            return View();
+        }
+    }
+}
