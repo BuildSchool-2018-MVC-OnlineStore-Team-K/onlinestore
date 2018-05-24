acc0e6ca40c6fc62acbf5736676c38edb524c8d8
耀文 郭 <karta1407617@gmail.com>
2018-05-24 16:47:24 +0800

    新增registerModel

---
+++ Delizioso/Models/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class RegisterModel
+    {
+        public string Name  { get; set; }
+        public string Address  { get; set; }
+        public DateTime Birthday  { get; set; }
+        public int CreditCard  { get; set; }
+        public string Phone  { get; set; }
+        public string Email  { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public int Block  { get; set; }
+        public string Career  { get; set; }
+
+
+}
+}
