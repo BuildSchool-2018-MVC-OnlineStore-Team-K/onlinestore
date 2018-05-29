2ff4c89bede664fc37a828e6a80c2a59996d8d38
丰羿 王 <tra202temu2000@gmail.com>
2018-05-29 12:39:28 +0800

    同步

---
+++ Delizioso/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Service;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("Product")]
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        [Route("{id}")]
+        public ActionResult SimpleProduct(int id)
+        {
+            var controller = new ProductsService();
+            var product = controller.GetProductDetail(id);
+            ViewBag.result = product;
+            return View();
+        }
+    }
+}
