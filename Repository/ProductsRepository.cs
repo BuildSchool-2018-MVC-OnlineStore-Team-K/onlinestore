d8258aac31193164a91739121080e11611de615b
耀文 郭 <karta1407617@gmail.com>
2018-05-30 17:34:34 +0800

    刪掉垃圾

--- Repository/ProductsRepository.cs
+++ Repository/ProductsRepository.cs
@@ -19,10 +19,6 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
 
         public void Update(Products model)
         {
-            //using (SqlConnection connection = new SqlConnection(source.connect))
-            //{
-            //    var exec = connection.Execute("UPDATE Products SET Category=@category, ProductName=@productname, UnitPrice=@unitprice, ShelfTime=@shelftime WHERE ProductID = @productid");
-            //}
             SqlConnection connection = new SqlConnection(source.connect);
             var sql = "UPDATE Products SET Category=@category, ProductName=@productname, UnitPrice=@unitprice, ShelfTime=@shelftime,Picture=@picture WHERE ProductID = @productid";
             SqlCommand command = new SqlCommand(sql, connection);
@@ -41,10 +37,6 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
 
         public void Delete(Products model)
         {
-            //using (SqlConnection connection = new SqlConnection(source.connect))
-            //{
-            //    var exec = connection.Execute("DELETE FROM Products WHERE ProductID = @productid");
-            //}  
             SqlConnection connection = new SqlConnection(source.connect);
             var sql = "Delete FROM Products WHERE ProductID=@productID";
 
@@ -67,31 +59,10 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
                 var result = connection.Query<Products>(query, Parameters);
                 return result;
             }
-            //"SELECT * FROM Products WHERE ProductID = @productid";
-
-            //SqlCommand command = new SqlCommand(sql, connection);
-            //command.Parameters.AddWithValue("@productid", ProductID);
-
-            //connection.Open();
-
-            //var list = new List<Products>();
-            //var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
-            //var products = new Products();
-
-            //while (reader.Read())
-            //{
-            //    products = DbReaderModelBinder<Products>.Bind(reader);
-            //    list.Add(products);
-            //}
-
-            //reader.Close();
-            //connection.Close();
+            
         }
 
-        /// <summary>
-        /// Get all products and show on ProductsPage
-        /// </summary>
-        /// <returns></returns>
+       
         public IEnumerable<Products> GetAll()
         {
             var query = "SELECT p.ProductID, (SELECT Category FROM Products WHERE ProductID = p.ProductID) AS Category,(SELECT ProductName FROM Products WHERE ProductID = p.ProductID) AS ProductName, (SELECT UnitPrice FROM Products WHERE ProductID = p.ProductID) AS UnitPrice,(SELECT ShelfTime FROM Products WHERE ProductID = p.ProductID) AS ShelfTime,(SELECT TOP 1 Discount FROM Discounts WHERE ProductID = p.ProductID AND EndTime >= GETDATE() ORDER BY StartTime DESC) AS Discount FROM Products p LEFT JOIN Discounts d ON d.ProductID = p.ProductID GROUP BY p.ProductID";
@@ -102,10 +73,7 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
             }            
         }
 
-        /// <summary>
-        /// 價格排序:低->高
-        /// </summary>
-        /// <returns></returns>
+   
         public IEnumerable<Products> OrderByUnitprice()
         {
             using (SqlConnection connection = new SqlConnection(source.connect))
@@ -155,9 +123,6 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
 
         public string GetProductName(int ProductID)  //查詢訂單、折扣排名(傳入產品ID，傳回產品名稱)
         {
-            //using (SqlConnection connection = new SqlConnection(source.connect))
-            //{ 
-            //    var result = connection.Query<Products>("SELECT ProductName FROM Products WHERE ProductID=@productid Group By ColorID").ToString();
             SqlConnection connection = new SqlConnection(source.connect);
             var sql = "SELECT ProductName FROM Products WHERE ProductID=@productid";
             SqlCommand command = new SqlCommand(sql, connection);
@@ -166,7 +131,6 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
 
             var reader = command.ExecuteReader();
             string result = "";
-            //Products products = new Products();
 
             while (reader.Read())
             {
@@ -176,7 +140,6 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
             reader.Close();
             connection.Close();
             return result;
-            //}
         }       
 
         public IEnumerable<Products> _GetAll()
@@ -189,34 +152,12 @@ namespace BuildSchool.MVCSolution.OnlineStore.Repository
 
             var reader = command.ExecuteReader();
             var list = new List<Products>();
-            //var properties = typeof(Products).GetProperties();
             Products products = new Products();
 
             while (reader.Read())
             {
                 products = DbReaderModelBinder<Products>.Bind(reader);
                 list.Add(products);
-                //products = new Products();
-                //for (var i = 0; i < reader.FieldCount; i++)
-                //{
-                //    var fieldName = reader.GetName(i);
-                //    var property = properties.FirstOrDefault(
-                //        p => p.Name == fieldName);
-
-                //    if (property == null)
-                //        continue;
-
-                //    if (!reader.IsDBNull(i))
-                //        property.SetValue(products,
-                //reader.GetValue(i));
-                //var product = new Products();
-                //product.ProductID = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("ProductID")));  //用欄位名稱取得位址
-                //product.Category = reader.GetValue(reader.GetOrdinal("Category")).ToString();
-                //product.ProductName = reader.GetValue(reader.GetOrdinal("ProductName")).ToString();
-                //product.UnitPrice = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("UnitPrice")));
-                //product.ShelfTime = Convert.ToDateTime(reader.GetValue(reader.GetOrdinal("ShelfTime")));
-                //products.Add(product);
-                //}
             }
 
             reader.Close();
