f571cae5948dec9b2552675da4fee63c72f4302a
Jay <shadle0307@gmail.com>
2018-05-30 19:12:57 +0800

    新增後臺產品管理layout，，1003個更變我是大ㄐㄐ

---
+++ Delizioso/Controllers/BackEndController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("backend")]
+    public class BackEndController : Controller
+    {
+        [Route("products")]
+        // GET: BackEnd
+        public ActionResult ProductsManage()
+        {
+            return View();
+        }
+    }
+}
