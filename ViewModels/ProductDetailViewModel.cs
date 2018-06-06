a31e012a120c4ae0929fcea5674dceeb6617d0c0
耀文 郭 <karta1407617@gmail.com>
2018-06-06 13:59:19 +0800

    我改了好多我只記得有照片上傳跟撈所有產品詳細資料到網頁上可是沒有美化請多多幫忙

---
+++ ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class ProductDetailViewModel
+    {
+        public string ProductID { get; set; }
+        public string ColorID { get; set; }
+        public string SizeID { get; set; }
+
+        public string  Category { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public string SizeType { get; set; }
+        public string Color { get; set; }
+        public int Stock { get; set; }
+    }
+}
