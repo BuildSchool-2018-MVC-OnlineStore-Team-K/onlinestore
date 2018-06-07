09cef7d6ca30b2c68f493827ae3962909a87e231
耀文 郭 <karta1407617@gmail.com>
2018-06-07 19:18:04 +0800

    阿伊屋欸喔

--- RepositoryTests/ProductsRepositoryTests.cs
+++ RepositoryTests/ProductsRepositoryTests.cs
@@ -40,12 +40,7 @@ namespace BuildSchool.MVCSolution.OnlineStore.ProductsRepositoryTests
             Assert.IsTrue(list.Count() > 0);
         }
 
-        [TestMethod()]
-        public void GetProductDetailTest()
-        {
-            var list = repo.GetProductDetail(1);
-            Assert.IsTrue(list.Count() > 0);
-        }
+
 
         [TestMethod()]
         public void OrderByShelfTimeDESCTest()
